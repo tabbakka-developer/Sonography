a7022e09323eb9376f3f18c96cd9b8a506e6efa4
Vladislav Popov <vlad.popov@webidea.com.ua>
2018-08-10 16:46:35 +0300

    hover make color

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,12 +4,10 @@
     @include('additional.css_links')
 </head>
 <body>
-
-@include('additional.top_menu')
-
-@yield('index')
-
-@include('additional.footer_index')
-
+<div class="body-wrapper">
+    @include('additional.top_menu')
+    @yield('index')
+    @include('additional.footer_index')
+</div>
 </body>
 </html>
