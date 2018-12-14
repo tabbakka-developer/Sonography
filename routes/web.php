e093bdd0b13653067a0cabd7b1452ed341a0e78d
Vladislav Popov <vlad.popov@webidea.com.ua>
2018-12-14 22:19:42 +0200

    зачем мне все это

--- routes/web.php
+++ routes/web.php
@@ -32,16 +32,16 @@ function checkAdminDomain() {
 
 //admin
 
-Route::get('/login', "Admin\Auth\LoginController@showForm");
-Route::get('/logout', "Admin\Auth\LoginController@logout");
-Route::post('/login', "Admin\Auth\LoginController@login");
-
-Route::group(['middleware' => 'auth'], function () {
-
-	Route::get('/dashboard', "Admin\DashBoardController@index");
-	Route::resource('products', "Admin\ProductsController");
-
-});
+//Route::get('/login', "Admin\Auth\LoginController@showForm");
+//Route::get('/logout', "Admin\Auth\LoginController@logout");
+//Route::post('/login', "Admin\Auth\LoginController@login");
+//
+//Route::group(['middleware' => 'auth'], function () {
+//
+//	Route::get('/dashboard', "Admin\DashBoardController@index");
+//	Route::resource('products', "Admin\ProductsController");
+//
+//});
 
 //checkAdminDomain();
 
@@ -50,29 +50,29 @@ Route::group(['middleware' => 'auth'], function () {
 //	return view('admin.dashboard');
 //});
 
-//Route::get('/pre', function () {
-//	return view('pre.index');
-//});
-//
-//Route::get('/', function () {
-//    return view('index.index');
-//})->name('main');
-//
-//Route::get('/news', function () {
-//	return view('index.news');
-//})->name('news');
-//
-//Route::get('/about', function () {
-//	return view('index.about');
-//})->name('about');
-//
-//Route::get('/category', function () {
-//	return view('index.category');
-//});
-//
-//Route::get('/item', function () {
-//	return view('index.item');
-//});
+Route::get('/pre', function () {
+	return view('pre.index');
+});
+
+Route::get('/', function () {
+    return view('index.index');
+})->name('main');
+
+Route::get('/news', function () {
+	return view('index.news');
+})->name('news');
+
+Route::get('/about', function () {
+	return view('index.about');
+})->name('about');
+
+Route::get('/category', function () {
+	return view('index.category');
+});
+
+Route::get('/item', function () {
+	return view('index.item');
+});
 
 
 
