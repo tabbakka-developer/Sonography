50a5831585878736dfa2c76493c82d4e58594435
Vladislav Popov <vlad.popov@webidea.com.ua>
2018-12-15 22:31:35 +0200

    routes upd

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,45 @@ function checkAdminDomain() {
 	$host = Request::getHost();
 	if ($host === 'xn--80aimpg.xn--80afqrebpd7a.xn--p1ai') {
 
-		Route::get('/', function() {
-			return view('admin.auth.login');
+		Route::get('/login', "Admin\Auth\LoginController@showForm");
+		Route::get('/logout', "Admin\Auth\LoginController@logout");
+		Route::post('/login', "Admin\Auth\LoginController@login");
+
+		Route::group(['middleware' => 'auth'], function () {
+
+			Route::get('/dashboard', "Admin\DashBoardController@index");
+			Route::resource('products', "Admin\ProductsController");
+
 		});
 
 	} elseif ($host === 'xn--80afqrebpd7a.xn--p1ai') {
 
-		Route::get('/', function() {
-			return Redirect::to('https://xn--80aimpg.xn--80afqrebpd7a.xn--p1ai/');
+//		Route::get('/', function() {
+//			return Redirect::to('https://xn--80aimpg.xn--80afqrebpd7a.xn--p1ai/');
+//		});
+
+		Route::get('/pre', function () {
+			return view('pre.index');
+		});
+
+		Route::get('/', function () {
+			return view('index.index');
+		})->name('main');
+
+		Route::get('/news', function () {
+			return view('index.news');
+		})->name('news');
+
+		Route::get('/about', function () {
+			return view('index.about');
+		})->name('about');
+
+		Route::get('/category', function () {
+			return view('index.category');
+		});
+
+		Route::get('/item', function () {
+			return view('index.item');
 		});
 
 	}
@@ -32,16 +63,7 @@ function checkAdminDomain() {
 
 //admin
 
-//Route::get('/login', "Admin\Auth\LoginController@showForm");
-//Route::get('/logout', "Admin\Auth\LoginController@logout");
-//Route::post('/login', "Admin\Auth\LoginController@login");
-//
-//Route::group(['middleware' => 'auth'], function () {
-//
-//	Route::get('/dashboard', "Admin\DashBoardController@index");
-//	Route::resource('products', "Admin\ProductsController");
-//
-//});
+
 
 //checkAdminDomain();
 
@@ -50,29 +72,29 @@ function checkAdminDomain() {
 //	return view('admin.dashboard');
 //});
 
-Route::get('/pre', function () {
-	return view('pre.index');
-});
-
-Route::get('/', function () {
-    return view('index.index');
-})->name('main');
-
-Route::get('/news', function () {
-	return view('index.news');
-})->name('news');
-
-Route::get('/about', function () {
-	return view('index.about');
-})->name('about');
-
-Route::get('/category', function () {
-	return view('index.category');
-});
-
-Route::get('/item', function () {
-	return view('index.item');
-});
+//Route::get('/pre', function () {
+//	return view('pre.index');
+//});
+//
+//Route::get('/', function () {
+//    return view('index.index');
+//})->name('main');
+//
+//Route::get('/news', function () {
+//	return view('index.news');
+//})->name('news');
+//
+//Route::get('/about', function () {
+//	return view('index.about');
+//})->name('about');
+//
+//Route::get('/category', function () {
+//	return view('index.category');
+//});
+//
+//Route::get('/item', function () {
+//	return view('index.item');
+//});
 
 
 
