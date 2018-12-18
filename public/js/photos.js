10c356ea5505546722edda1946374019336e88d7
Vladislav Popov <vlad.popov@webidea.com.ua>
2018-12-18 15:27:30 +0200

    photo validation fix

--- public/js/photos.js
+++ public/js/photos.js
@@ -8,7 +8,8 @@ $(document).ready(function () {
             url:'/photos/makers',
             dataType: 'JSON',
             success: function (data) {
-                console.log(data);
+                let makers = JSON.parse(data);
+                console.log(makers);
             },
             error: function (data) {
                 console.log(data);
