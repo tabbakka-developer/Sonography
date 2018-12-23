42c70922366537e3085143584da62c210fee2a10
Vladislav Popov <vlad.popov@webidea.com.ua>
2018-12-23 16:13:35 +0200

    thumbs and watermarks part_4

--- resources/views/admin/photos/specific.blade.php
+++ resources/views/admin/photos/specific.blade.php
@@ -35,7 +35,7 @@
                 <div class="row">
 
                     @foreach($photos as $photo)
-                        {{ dd($photo) }}
+                        {{ dd($photo->thumb) }}
                         <div class="card" style="width: 15rem">
                             <img src="{{ asset('storage/' . $photo->thumb) }}" class="card-img-top">
                             <div class="card-body">
