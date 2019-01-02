66e23cf69830d3b34d4c310dd8deb9e0f70f5ecb
Vladislav Popov <vlad.popov@webidea.com.ua>
2019-01-02 15:44:08 +0200

    minor

--- app/Product.php
+++ app/Product.php
@@ -46,8 +46,8 @@ class Product extends Model implements Interfaces\Product {
 	}
 
 	public static function prices() {
-		$prices_rub = self::selectRaw('DISTINCT price WHERE currency = RUB')->get();
-		$prices_usd = self::selectRaw('DISTINCT price WHERE currency = USD')->get();
+		$prices_rub = self::selectRaw("DISTINCT price WHERE currency = 'RUB'")->get();
+		$prices_usd = self::selectRaw("DISTINCT price WHERE currency = 'USD'")->get();
 		return [
 			'USD' => $prices_usd,
 			'RUB' => $prices_rub
