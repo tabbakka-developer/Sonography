9e3f97339bf20872762730a66482177e70b6210f
Vladislav Popov <vlad.popov@webidea.com.ua>
2019-01-02 15:59:23 +0200

    minor

--- resources/views/additional/side_menu.blade.php
+++ resources/views/additional/side_menu.blade.php
@@ -2,67 +2,69 @@
 
 <div id="side-var-vue">
     <side-bar>
-        <button class="dropdown-btn">
-            <span>Фирма</span>
-            <i class="fas fa-plus"></i>
-        </button>
-        <div class="dropdown-container">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
-        </div>
-
-        <button class="dropdown-btn">Статус
-            <i class="fas fa-plus"></i>
-        </button>
-        <div class="dropdown-container">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
-        </div>
-
-        <button class="dropdown-btn">Качество
-            <i class="fas fa-plus"></i>
-        </button>
-        <div class="dropdown-container">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
-        </div>
-
-        <button class="dropdown-btn">Страна
-            <i class="fas fa-plus"></i>
-        </button>
-        <div class="dropdown-container">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
-        </div>
-
-        <button class="dropdown-btn">Сортировка
-            <i class="fas fa-plus"></i>
-        </button>
-        <div class="dropdown-container">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
-        </div>
-
-        <button class="dropdown-btn">Диапозон цены
-            <i class="fas fa-plus"></i>
-        </button>
-        <div class="dropdown-container">
-            <div class="price-block">
-                <span class="price-min"></span>
-                <span class="price-separ">-</span>
-                <span class="price-max"></span>
-            </div>
-
-            <div id="slider"></div>
-        </div>
+        <h5>????!?!!?!?</h5>
     </side-bar>
 </div>
 
+
+{{--<button class="dropdown-btn">--}}
+    {{--<span>Фирма</span>--}}
+    {{--<i class="fas fa-plus"></i>--}}
+{{--</button>--}}
+{{--<div class="dropdown-container">--}}
+    {{--<a href="#">Link 1</a>--}}
+    {{--<a href="#">Link 2</a>--}}
+    {{--<a href="#">Link 3</a>--}}
+{{--</div>--}}
+
+{{--<button class="dropdown-btn">Статус--}}
+    {{--<i class="fas fa-plus"></i>--}}
+{{--</button>--}}
+{{--<div class="dropdown-container">--}}
+    {{--<a href="#">Link 1</a>--}}
+    {{--<a href="#">Link 2</a>--}}
+    {{--<a href="#">Link 3</a>--}}
+{{--</div>--}}
+
+{{--<button class="dropdown-btn">Качество--}}
+    {{--<i class="fas fa-plus"></i>--}}
+{{--</button>--}}
+{{--<div class="dropdown-container">--}}
+    {{--<a href="#">Link 1</a>--}}
+    {{--<a href="#">Link 2</a>--}}
+    {{--<a href="#">Link 3</a>--}}
+{{--</div>--}}
+
+{{--<button class="dropdown-btn">Страна--}}
+    {{--<i class="fas fa-plus"></i>--}}
+{{--</button>--}}
+{{--<div class="dropdown-container">--}}
+    {{--<a href="#">Link 1</a>--}}
+    {{--<a href="#">Link 2</a>--}}
+    {{--<a href="#">Link 3</a>--}}
+{{--</div>--}}
+
+{{--<button class="dropdown-btn">Сортировка--}}
+    {{--<i class="fas fa-plus"></i>--}}
+{{--</button>--}}
+{{--<div class="dropdown-container">--}}
+    {{--<a href="#">Link 1</a>--}}
+    {{--<a href="#">Link 2</a>--}}
+    {{--<a href="#">Link 3</a>--}}
+{{--</div>--}}
+
+{{--<button class="dropdown-btn">Диапозон цены--}}
+    {{--<i class="fas fa-plus"></i>--}}
+{{--</button>--}}
+{{--<div class="dropdown-container">--}}
+    {{--<div class="price-block">--}}
+        {{--<span class="price-min"></span>--}}
+        {{--<span class="price-separ">-</span>--}}
+        {{--<span class="price-max"></span>--}}
+    {{--</div>--}}
+
+    {{--<div id="slider"></div>--}}
+{{--</div>--}}
 
 
 
