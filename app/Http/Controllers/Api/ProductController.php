6a51d300c4eea50e501eb61dd176d8910695fddb
Vladislav Popov <vlad.popov@webidea.com.ua>
2019-01-02 13:22:09 +0200

    minor

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -13,6 +13,7 @@ class ProductController extends Controller {
 		switch ($request->category) {
 
 			case 'uzi':
+				dd(UziApparat::prices());
 				return response()->json([
 					'success' => true,
 					'prices' => UziApparat::prices()
